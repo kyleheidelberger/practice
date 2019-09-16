4397d21f4b7df7edd79aecd683c557ff1ab2df64
Kyle Heidelberger <kyle.heidelberger@gmail.com>
2019-09-16 12:30:13 -0400

    add post

--- instagram-clone/src/App.js
+++ instagram-clone/src/App.js
@@ -1,10 +1,16 @@
-import React, { Component } from 'react';
-import './App.css';
-import Header from './components/Header';
+import React, { Component } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Post from './components/Post';
 class App extends Component {
   render() {
     return (
-      <Header />
+      <div>
+        <Header />
+        <div>
+          <Post />
+        </div>
+      </div>
     );
   }
 }
