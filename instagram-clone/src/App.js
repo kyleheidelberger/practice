b39716615b43fe709e8bf374767472832dbb7894
Kyle Heidelberger <kyle.heidelberger@gmail.com>
2019-09-16 12:34:00 -0400

    done

--- instagram-clone/src/App.js
+++ instagram-clone/src/App.js
@@ -1,17 +1,20 @@
-import React, { Component } from "react";
-import "./App.css";
-import Header from "./components/Header";
+import React, { Component } from 'react';
+import './App.css';
+import Header from './components/Header';
 import Post from './components/Post';
+
 class App extends Component {
   render() {
-    return (
-      <div>
+    return <div className="App">
         <Header />
-        <div>
-          <Post />
-        </div>
-      </div>
-    );
+        <section className="App-main">
+          <Post nickname="Chris" avatar="https://www.laravelnigeria.com/img/chris.jpg" caption="Moving the community!" image="https://pbs.twimg.com/media/DOXI0IEXkAAkokm.jpg" />
+          <Post nickname="OG" avatar="https://www.laravelnigeria.com/img/chris.jpg" caption="Holding a mic" image="https://pbs.twimg.com/media/DOXI0IEXkAAkokm.jpg" />
+
+          {/* more posts */}
+        </section>
+      </div>;
   }
 }
+
 export default App;
